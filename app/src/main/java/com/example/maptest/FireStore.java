c4a839c2c09108ba027d58ea1918382006333e87
nicholasmf <lokiloki0@hotmail.com>
2019-03-20 01:17:28 -0300

    removed unused lines

--- app/src/main/java/com/example/maptest/FireStore.java
+++ app/src/main/java/com/example/maptest/FireStore.java
@@ -1,6 +1,5 @@
 package com.example.maptest;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
@@ -8,7 +7,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.android.gms.maps.model.LatLng;
