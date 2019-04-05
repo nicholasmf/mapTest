c33b562a2f17780c5946114190ae58be953192ab
nicholasmf <lokiloki0@hotmail.com>
2019-04-05 03:41:32 -0300

    panel fragment

--- app/src/main/java/com/example/maptest/MapsActivity.java
+++ app/src/main/java/com/example/maptest/MapsActivity.java
@@ -75,7 +75,7 @@ public class MapsActivity extends AppCompatActivity
             mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
         }
 
-        setContentView(R.layout.activity_maps);
+mmit -m ""        setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
